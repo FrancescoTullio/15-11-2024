6fed917da8cb4aa39c6f8e750ad5104a509234dc
FrancescoTullio <francescotullio1992@gmail.com>
2024-11-15 13:38:00 +0100

    feat:last

--- js-jsnacks-blocco-5/snack-8/script.js
+++ js-jsnacks-blocco-5/snack-8/script.js
@@ -21,3 +21,11 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const marcosClass = students.find((curStudent) => {
+  if(curStudent.name === "Marco Lanci"){
+    return curStudent;
+  }
+})
+
+console.log(marcosClass.class);
